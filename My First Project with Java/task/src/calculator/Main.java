4233580cba4201271f2ad222df77ea559ba8c720
tobiAnime <tobi@sharkscode.com>
2024-12-29 22:27:20 +0100

    My first project in Java

--- My First Project with Java/task/src/calculator/Main.java
+++ My First Project with Java/task/src/calculator/Main.java
@@ -1,13 +1,31 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prices!");
-        System.out.println("Bubblegum: $2");
-        System.out.println("Toffee: $0.2");
-        System.out.println("Ice cream: $5");
-        System.out.println("Milk chocolate: $4");
-        System.out.println("Doughnut: $2.5");
-        System.out.println("Pancake: $3.2");
+
+        Scanner in = new Scanner(System.in);
+        int income = 202 + 118 + 2250 + 1680 + 1075 + 80;
+
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $202");
+        System.out.println("Toffee: $118");
+        System.out.println("Ice cream: $2250");
+        System.out.println("Milk chocolate: $1680");
+        System.out.println("Doughnut: $1075");
+        System.out.println("Pancake: $80");
+        System.out.println();
+        System.out.println("Income: $" + income);
+
+        System.out.println("Staff expenses:");
+        int expenses = in.nextInt();
+        System.out.println("Other expenses:");
+        int otherExpenses = in.nextInt();
+
+        int netIncome = income - expenses - otherExpenses;
+        System.out.println("Net income: $" + netIncome);
+
+
     }
 }
